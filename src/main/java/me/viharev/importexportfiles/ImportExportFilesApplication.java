f5b53b7bab5e74eab8263c7591734997dd2d9ccb
ViharevN <swix_off@mail.ru>
2022-12-19 20:21:10 +0300

    commit

---
+++ src/main/java/me/viharev/importexportfiles/ImportExportFilesApplication.java
@@ -0,0 +1,13 @@
+package me.viharev.importexportfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportExportFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImportExportFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/viharev/importexportfiles/ImportExportFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.importexportfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportExportFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
